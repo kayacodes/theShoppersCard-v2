4b379f7a0a7d1cc065448e2d3668c01d43f03bf5
kayacodes <kayadokrat@gmail.com>
2020-09-18 00:15:20 +0100

    Database setup

--- PHP_Includes/dbh.php
+++ PHP_Includes/dbh.php
@@ -1,8 +1,8 @@
 <?php
 
-$servername = "localhost:3308";
-$dbUsername = "root";
-$dbPassword = "";
+$servername = "eu-cdbr-west-03.cleardb.net";
+$dbUsername = "b19d128e414970";
+$dbPassword = "a7636806";
 $dbName = "user_registration";
 
 $conn = mysqli_connect($servername, $dbUsername, $dbPassword, $dbName);
