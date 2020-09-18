1b39575c9e2ee6eeeee6a87935682311763f6a21
kayacodes <kayadokrat@gmail.com>
2020-09-18 02:30:19 +0100

    json

--- PHP_Includes/dbh.php
+++ PHP_Includes/dbh.php
@@ -1,13 +1,30 @@
 <?php
 
-$servername = "localhost:3308";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "user_registration";
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"], 1);
 
-$conn = mysqli_connect($servername, $dbUsername, $dbPassword, $dbName);
+$active_group = 'default';
+$active_record = TRUE;
+
+$db['default']['hostname'] = $cleardb_server;
+$db['default']['username'] = $cleardb_username;
+$db['default']['password'] = $cleardb_password;
+$db['default']['database'] = $cleardb_db;
+
+
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 if (!$conn) {
     die("Connection Failed: ".mysqli_connect_error());
 }
+
+/*$servername = "localhost:3308";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "user_registration";
+
+*/
 ?>
