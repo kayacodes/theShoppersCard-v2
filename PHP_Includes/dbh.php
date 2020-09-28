66b54e1be22096f54a8e2668ac9700c14f79bd42
kayacodes <kayadokrat@gmail.com>
2020-09-28 18:52:05 +0100

    ID Validation, DBH Update

--- PHP_Includes/dbh.php
+++ PHP_Includes/dbh.php
@@ -9,16 +9,10 @@ $db = substr($url["path"], 1);
 
 $conn = mysqli_connect($server, $username, $dbPassword, $db);
 
-/*$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);*/
 
 if (!$conn) {
     die("Connection Failed: ".mysqli_connect_error());
 }
 
-/*$servername = "localhost:3308";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "user_registration";
 
-*/
 ?>
